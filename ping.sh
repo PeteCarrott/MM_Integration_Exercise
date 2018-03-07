fd8c947d752b9188f4d9469c6599d68ed10968c0
Minh Tien TO <minhtien.to@altran.com>
2018-03-07 17:45:18 +0100

    updated script ping.sh: now user can path the ip address

--- ping.sh
+++ ping.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+
+function pingServer {
+	while [ $loop == true ]; do
+		echo "what is the server ip address you want to ping?"
+		read ipAddress
+		ping -c 1 $ipAddress
+		if [ $? == 0 ]; then
+			echo "server reachable"
+		else
+			echo "server not reachable"
+		fi
+		echo "Do you want to ping another server? Y/N"
+			read response
+			neg="N"
+			if [ $response == $neg ]; then
+				loop=false
+				echo "Application terminated"
+			fi
+	done
+}
+
 echo "application started"
 echo "try to ping the server connected via usb"
-ping -c 139.128.8.243
-if [ $? == 0 ]; then
-	echo "server reachable"
-else
-	echo "server not reachable"
-fi
+loop=true
+pingServer
 	
